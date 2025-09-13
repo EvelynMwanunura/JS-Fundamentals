71b9833cd9b2178137908d9d8cba2f2950cd52d5
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-09-13 21:45:12 +0200

    Add arguments

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+const arguments = process.argv.slice(2);
+if (arguments.length === 0) {
+  console.log("No arguments");
+} else if (arguments.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}

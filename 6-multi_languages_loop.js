ae9daf815977d79039b27420d5123ddd8a079706
EvelynMwanunura <evelynmwanunura@gmail.com>
2025-09-13 21:57:53 +0200

    Using loops

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+let output = "";
+for (let i = 0; i < languages.length; i++) {
+  output += languages[i];
+  if (i < languages.length - 1) output += "\n"; // add newline except after last
+}
+
+console.log(output);
